746e5e37a909154f164ad970b88d08576309bd48
jtillson1 <65985044+jtillson1@users.noreply.github.com>
2020-08-22 14:15:15 -0500

    ...

--- server.js
+++ server.js
@@ -1,24 +1,17 @@
 const express = require('express');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
-
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Default response for any other request(Not Found) Catch all
+// Default response for any other requests(Not Found) Catch all
 app.use((req, res) => {
-    res.status(404).end();
-  });
-
-app.get('/', (req, res) => {
-    res.json({
-        message: 'hello bitch'
-    });
+  res.status(404).end();
 });
 
-
 app.listen(PORT, () => {
-    console.log(`Server now zippin around on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`);
 });
