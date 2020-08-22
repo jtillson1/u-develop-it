188b759e04d53a7373ebb9112a5805fb234d2ded
jtillson1 <65985044+jtillson1@users.noreply.github.com>
2020-08-22 14:10:05 -0500

    intiial server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server now runnong on port ${PORT}`)
+});
