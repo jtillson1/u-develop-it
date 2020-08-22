d62a800980f15a47765e5aceb8ce577c879bc2b8
jtillson1 <65985044+jtillson1@users.noreply.github.com>
2020-08-22 14:13:32 -0500

    route to handler user requests that arent supported by the app

--- server.js
+++ server.js
@@ -7,10 +7,18 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Default response for any other request(Not Found) Catch all
+app.use((req, res) => {
+    res.status(404).end();
+  });
 
-
+app.get('/', (req, res) => {
+    res.json({
+        message: 'hello bitch'
+    });
+});
 
 
 app.listen(PORT, () => {
-    console.log(`Server now runnong on port ${PORT}`)
+    console.log(`Server now zippin around on port ${PORT}`)
 });
